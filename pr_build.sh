e3b070048e82f5035288e1ec1ae4786384e8ffc5
Marc Page <marcallenpage@gmail.com>
2021-12-30 21:03:28 -0600

    Echoing commands run

--- pr_build.sh
+++ pr_build.sh
@@ -16,9 +16,10 @@ pip3 install -qr Requirements.txt
 if [ "$1" = "" ]; then export CHECK=--check; fi
 
 export SOURCES="src/*.py"
-echo '##[group] Running black python source validation'
+echo "##[group] Running black python source validation"
+echo "##[command] black $CHECK $SOURCES"
 black $CHECK $SOURCES
-echo '##[endgroup]'
+echo "##[endgroup]"
 export BLACK_STATUS=$?
 if [ $BLACK_STATUS -ne 0 ]; then
     echo "💥💥 Please run black on this source to reformat and resubmit 💥💥 "
@@ -26,9 +27,10 @@ else
     echo "✅ black verification successful"
 fi
 
-echo '##[group] Running pylint python source validation'
+echo "##[group] Running pylint python source validation"
+echo "##[command] pylint $SOURCES"
 pylint $SOURCES
-echo '##[endgroup]'
+echo "##[endgroup]"
 export PYLINT_STATUS=$?
 if [ $PYLINT_STATUS -ne 0 ]; then
     echo "💥💥 Please fix the above pylint errors and resubmit 💥💥 "
@@ -36,9 +38,10 @@ else
     echo "✅ pylint verification successful"
 fi
 
-echo '##[group] Running flake8 python source validation'
+echo "##[group] Running flake8 python source validation"
+echo "##[command] flake8 --max-line-length=100 $SOURCES"
 flake8 --max-line-length=100 $SOURCES
-echo '##[endgroup]'
+echo "##[endgroup]"
 export FLAKE8_STATUS=$?
 if [ $PYLINT_STATUS -ne 0 ]; then
     echo "💥💥 Please fix the above flake8 errors and resubmit 💥💥 "
@@ -46,9 +49,10 @@ else
     echo "✅ flake8 verification successful"
 fi
 
-echo '##[group] Running python unit tests'
+echo "##[group] Running python unit tests"
+echo "##[command] python3 -m unittest discover -s src/tests -t src"
 python3 -m unittest discover -s src/tests -t src
-echo '##[endgroup]'
+echo "##[endgroup]"
 export TEST_STATUS=$?
 if [ $TEST_STATUS -ne 0 ]; then
     echo "💥💥 Please fix the above test failures and resubmit 💥💥 "
