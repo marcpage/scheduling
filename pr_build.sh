8295481bd565bbc76bf2ef0e09af3c9f0555355c
Marc Page <marcallenpage@gmail.com>
2021-12-30 21:22:08 -0600

    More GitHub log formatting for build_pr.sh (#44)
    * Fixing pr test output
    
    * Fixing code
    
    * Converting SAWarnings to github warnings

--- pr_build.sh
+++ pr_build.sh
@@ -17,45 +17,45 @@ if [ "$1" = "" ]; then export CHECK=--check; fi
 
 export SOURCES="src/*.py"
 echo "##[group] Running black python source validation"
-echo "##[command] black $CHECK $SOURCES"
+echo "##[command]black $CHECK $SOURCES"
 black $CHECK $SOURCES
-echo "##[endgroup]"
 export BLACK_STATUS=$?
+echo "##[endgroup]"
 if [ $BLACK_STATUS -ne 0 ]; then
-    echo "💥💥 Please run black on this source to reformat and resubmit 💥💥 "
+    echo "##[error]💥💥 Please run black on this source to reformat and resubmit 💥💥 "
 else
     echo "✅ black verification successful"
 fi
 
 echo "##[group] Running pylint python source validation"
-echo "##[command] pylint $SOURCES"
+echo "##[command]pylint $SOURCES"
 pylint $SOURCES
-echo "##[endgroup]"
 export PYLINT_STATUS=$?
+echo "##[endgroup]"
 if [ $PYLINT_STATUS -ne 0 ]; then
-    echo "💥💥 Please fix the above pylint errors and resubmit 💥💥 "
+    echo "##[error]💥💥 Please fix the above pylint errors and resubmit 💥💥 "
 else
     echo "✅ pylint verification successful"
 fi
 
 echo "##[group] Running flake8 python source validation"
-echo "##[command] flake8 --max-line-length=100 $SOURCES"
+echo "##[command]flake8 --max-line-length=100 $SOURCES"
 flake8 --max-line-length=100 $SOURCES
-echo "##[endgroup]"
 export FLAKE8_STATUS=$?
+echo "##[endgroup]"
 if [ $PYLINT_STATUS -ne 0 ]; then
-    echo "💥💥 Please fix the above flake8 errors and resubmit 💥💥 "
+    echo "##[error]💥💥 Please fix the above flake8 errors and resubmit 💥💥 "
 else
     echo "✅ flake8 verification successful"
 fi
 
 echo "##[group] Running python unit tests"
-echo "##[command] python3 -m unittest discover -s src/tests -t src"
-python3 -m unittest discover -s src/tests -t src
-echo "##[endgroup]"
+echo "##[command]python3 -m unittest discover -s src/tests -t src"
+python3 -m unittest discover -s src/tests -t src 2>&1 | sed "/SAWarning:/s/^/##[warning]/"
 export TEST_STATUS=$?
+echo "##[endgroup]"
 if [ $TEST_STATUS -ne 0 ]; then
-    echo "💥💥 Please fix the above test failures and resubmit 💥💥 "
+    echo "##[error]💥💥 Please fix the above test failures and resubmit 💥💥 "
 else
     echo "✅ unit tests passed"
 fi
