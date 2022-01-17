aaada9b475bac98bd8470380742061d65175e965
Marc Page <marcallenpage@gmail.com>
2022-01-17 11:21:49 -0600

    Displaying shifts available for scheduling (#84)

--- src/ui/shift_scheduling.js
+++ src/ui/shift_scheduling.js
@@ -65,24 +65,35 @@ function shift_range(shift) {
     return days_between_dates(parse_date(shift.start_date), parse_date(shift.end_date));
 }
 
-/*
-    Get a list of non-overlapping shifts for the date.
-    TODO: This does not work right now
-*/
+function overlapping_shift(shift, shifts) {
+    for (var i = 0; i < shifts.length; ++i) {
+        if (shift.start_time >= shifts[i].start_time
+            && shift.start_time < shifts[i].end_time) {
+            return true;
+        }
+        if (shift.end_time >= shifts[i].start_time
+            && shift.end_time < shifts[i].end_time) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function get_shifts_on_day(shifts, date) {
     var day_of_week = (date.getDay() + 6) % 7;
     var shifts_on_day = shifts.filter(s => s.day_of_week == day_of_week);
+    var found_shifts = [];
 
-    shifts_on_day.sort((e1, e2) => e1.start_time < e2.start_time ? -1 : (e1.start_time > e2.start_time ? 1 : 0));
-    var best_shift = undefined;
+    // start with latest created get first non-overlapping shifts
+    shifts_on_day.sort((s1, s2) => s1.id > s2.id ? -1 : 1)
     for (var i = 0; i < shifts_on_day.length; ++i) {
-        var evaluate = shifts_on_day[i];
+        var shift = shifts_on_day[i];
 
-        if (!best_shift || shift_range(evaluate) < shift_range(best_shift)) {
-            best_shift = evaluate;
+        if (!overlapping_shift(shift, found_shifts)) {
+            found_shifts.push(shift);
         }
     }
-    return shifts;
+    return found_shifts.sort((s1,s2) => s1.start_time < s2.start_time ? -1 : 1);
 }
 
 function get_shifts(shifts, date, days) {
@@ -99,10 +110,60 @@ function get_shifts(shifts, date, days) {
     return found_shifts;
 }
 
+function minutes_to_time_string(m) {
+    var hours = Math.floor(m/60);
+    var minutes = m - hours * 60;
+    var ampm = hours < 12 ? "AM" : "PM";
+
+    if (hours > 12) {
+        hours -= 12;
+    } else if (hours == 0) {
+        hours = 12;
+    }
+    return hours.toString(10) + ":" + (minutes < 10 ? "0": "") + minutes.toString(10) + " " + ampm;
+}
+
+/*
+    If the shift list is empty, there are no shifts.
+    If the shift list doesn't have any shifts with roles, there are no shifts.
+*/
+function any_shifts(shift_list) {
+    for (var shift_index = 0; shift_index < shift_list.length; ++shift_index) {
+        if (shift_list[shift_index].roles.length > 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function update_shift_editor(employees, shifts, div_id) {
     var container = document.getElementById(div_id);
     var start_date = new Date();
 
     start_date.setDate(start_date.getDate() + 1);
-    container.innerText = get_shifts(shifts, start_date, 14);
+    var two_weeks_shifts = get_shifts(shifts, start_date, 14);
+    var display_text = "";
+    for (var day_index = 0; day_index < two_weeks_shifts.length; ++day_index) {
+        var day_shifts = two_weeks_shifts[day_index];
+        if (any_shifts(day_shifts.shifts_on_date)) {
+            display_text += "Date: " + day_shifts.date + "\n<ul>\n";
+            for (var shift_index = 0; shift_index < day_shifts.shifts_on_date.length; ++shift_index) {
+                var shift = day_shifts.shifts_on_date[shift_index];
+                if (shift.roles.length) {
+                    display_text += "<li>" + minutes_to_time_string(shift.start_time)
+                                           + " - "
+                                           + minutes_to_time_string(shift.end_time)
+                                           + "\n<ul>\n";
+
+                    for (var role_index = 0; role_index < shift.roles.length; ++role_index) {
+                        role = shift.roles[role_index];
+                        display_text += "<li>" + role.number.toString(10) + " x " + role.role + "</li>\n";
+                    }
+                    display_text += "</ul>\n</li>\n";
+                }
+            }
+            display_text += "</ul>";
+        }
+    }
+    container.innerHTML = display_text;
 }
