0440e6e16fe06369e549b80eaa2d9ed6406a3e15
Marc Page <MarcAllenPage@gmail.com>
2022-01-17 11:55:21 -0600

    cleaning up the js to be more standards compliant

--- src/ui/shift_scheduling.js
+++ src/ui/shift_scheduling.js
@@ -52,118 +52,116 @@ var shifts = [
     Find the
 */
 
-function parse_date(date_string) {
-    var parts = date_string.match(/(\d+)/g);
+function parseDate (dateString) {
+    const parts = dateString.match(/(\d+)/g);
     return new Date(parseInt(parts[0]), parseInt(parts[1]) - 1, parseInt(parts[2]));
 }
 
-function days_between_dates(d1, d2) {
+function daysBetweenDates (d1, d2) {
     return Math.round(Math.abs(d2.getTime() - d1.getTime()) / (1000 * 60 * 60 * 24));
 }
 
-function shift_range(shift) {
-    return days_between_dates(parse_date(shift.start_date), parse_date(shift.end_date));
+function shiftRange (shift) {
+    return daysBetweenDates(parseDate(shift.start_date), parseDate(shift.end_date));
 }
 
-function overlapping_shift(shift, shifts) {
-    for (var i = 0; i < shifts.length; ++i) {
-        if (shift.start_time >= shifts[i].start_time
-            && shift.start_time < shifts[i].end_time) {
+function overlappingShift (shift, shifts) {
+    for (let i = 0; i < shifts.length; ++i) {
+        if (shift.start_time >= shifts[i].start_time && shift.start_time < shifts[i].end_time) {
             return true;
         }
-        if (shift.end_time >= shifts[i].start_time
-            && shift.end_time < shifts[i].end_time) {
+        if (shift.end_time >= shifts[i].start_time && shift.end_time < shifts[i].end_time) {
             return true;
         }
     }
     return false;
 }
 
-function get_shifts_on_day(shifts, date) {
-    var day_of_week = (date.getDay() + 6) % 7;
-    var shifts_on_day = shifts.filter(s => s.day_of_week == day_of_week);
-    var found_shifts = [];
+function getShiftsOnDay (shifts, date) {
+    const dayOfWeek = (date.getDay() + 6) % 7;
+    const shiftsOnDay = shifts.filter(s => s.day_of_week === dayOfWeek);
+    const foundShifts = [];
 
     // start with latest created get first non-overlapping shifts
-    shifts_on_day.sort((s1, s2) => s1.id > s2.id ? -1 : 1)
-    for (var i = 0; i < shifts_on_day.length; ++i) {
-        var shift = shifts_on_day[i];
+    shiftsOnDay.sort((s1, s2) => s1.id > s2.id ? -1 : 1)
+    for (let i = 0; i < shiftsOnDay.length; ++i) {
+        const shift = shiftsOnDay[i];
 
-        if (!overlapping_shift(shift, found_shifts)) {
-            found_shifts.push(shift);
+        if (!overlappingShift(shift, foundShifts)) {
+            foundShifts.push(shift);
         }
     }
-    return found_shifts.sort((s1,s2) => s1.start_time < s2.start_time ? -1 : 1);
+    return foundShifts.sort((s1, s2) => s1.start_time < s2.start_time ? -1 : 1);
 }
 
-function get_shifts(shifts, date, days) {
-    var found_shifts = [];
+function getShifts (shifts, date, days) {
+    const foundShifts = [];
 
     while (days > 0) {
-        var date = new Date(date.valueOf());
-        var shifts_on_day = get_shifts_on_day(shifts, date);
-        var entry = {'date': date, 'shifts_on_date': shifts_on_day};
-        found_shifts.push(entry);
+        date = new Date(date.valueOf());
+        const shiftsOnDay = getShiftsOnDay(shifts, date);
+        const entry = { date: date, shifts_on_date: shiftsOnDay };
+        foundShifts.push(entry);
         days -= 1;
         date.setDate(date.getDate() + 1);
     };
-    return found_shifts;
+    return foundShifts;
 }
 
-function minutes_to_time_string(m) {
-    var hours = Math.floor(m/60);
-    var minutes = m - hours * 60;
-    var ampm = hours < 12 ? "AM" : "PM";
+function minutesToTimeString (m) {
+    let hours = Math.floor(m / 60);
+    const minutes = m - hours * 60;
+    const ampm = hours < 12 ? 'AM' : 'PM';
 
     if (hours > 12) {
         hours -= 12;
-    } else if (hours == 0) {
+    } else if (hours === 0) {
         hours = 12;
     }
-    return hours.toString(10) + ":" + (minutes < 10 ? "0": "") + minutes.toString(10) + " " + ampm;
+    return hours.toString(10) + ':' + (minutes < 10 ? '0' : '') + minutes.toString(10) + ' ' + ampm;
 }
 
 /*
     If the shift list is empty, there are no shifts.
     If the shift list doesn't have any shifts with roles, there are no shifts.
 */
-function any_shifts(shift_list) {
-    for (var shift_index = 0; shift_index < shift_list.length; ++shift_index) {
-        if (shift_list[shift_index].roles.length > 0) {
+function anyShifts (shiftList) {
+    for (let shiftIndex = 0; shiftIndex < shiftList.length; ++shiftIndex) {
+        if (shiftList[shiftIndex].roles.length > 0) {
             return true;
         }
     }
     return false;
 }
 
-function update_shift_editor(employees, shifts, div_id) {
-    var container = document.getElementById(div_id);
-    var start_date = new Date();
+function updateShiftEditor (employees, shifts, divId) {
+    const container = document.getElementById(divId);
+    const startDate = new Date();
 
-    start_date.setDate(start_date.getDate() + 1);
-    var two_weeks_shifts = get_shifts(shifts, start_date, 14);
-    var display_text = "";
-    for (var day_index = 0; day_index < two_weeks_shifts.length; ++day_index) {
-        var day_shifts = two_weeks_shifts[day_index];
-        if (any_shifts(day_shifts.shifts_on_date)) {
-            display_text += "Date: " + day_shifts.date + "\n<ul>\n";
-            for (var shift_index = 0; shift_index < day_shifts.shifts_on_date.length; ++shift_index) {
-                var shift = day_shifts.shifts_on_date[shift_index];
+    startDate.setDate(startDate.getDate() + 1);
+    const twoWeeksShifts = getShifts(shifts, startDate, 14);
+    let displayText = '';
+    for (let dayIndex = 0; dayIndex < twoWeeksShifts.length; ++dayIndex) {
+        const dayShifts = twoWeeksShifts[dayIndex];
+        if (anyShifts(dayShifts.shifts_on_date)) {
+            displayText += 'Date: ' + dayShifts.date + '\n<ul>\n';
+            for (let shiftIndex = 0; shiftIndex < dayShifts.shifts_on_date.length; ++shiftIndex) {
+                const shift = dayShifts.shifts_on_date[shiftIndex];
                 if (shift.roles.length) {
-                    display_text += "<li>" + minutes_to_time_string(shift.start_time)
-                                           + " - "
-                                           + minutes_to_time_string(shift.end_time)
-                                           + "\n<ul>\n";
+                    displayText += '<li>' + minutesToTimeString(shift.start_time) +
+                                           ' - ' +
+                                           minutesToTimeString(shift.end_time) +
+                                           '\n<ul>\n';
 
-                    for (var role_index = 0; role_index < shift.roles.length; ++role_index) {
-                        role = shift.roles[role_index];
-                        display_text += "<li>" + role.number.toString(10) + " x " + role.role + "</li>\n";
+                    for (let roleIndex = 0; roleIndex < shift.roles.length; ++roleIndex) {
+                        const role = shift.roles[roleIndex];
+                        displayText += '<li>' + role.number.toString(10) + ' x ' + role.role + '</li>\n';
                     }
-                    display_text += "</ul>\n</li>\n";
+                    displayText += '</ul>\n</li>\n';
                 }
             }
-            display_text += "</ul>";
+            displayText += '</ul>';
         }
     }
-    container.innerHTML = display_text;
+    container.innerHTML = displayText;
 }
